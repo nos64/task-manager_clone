70ef056a7cda6d2c50556c8b445217cd80815051
Mikhail Nosov <manosov1984@gmail.com>
2022-11-10 02:32:31 +0400

    feat: add button, input, task mixns

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import style from './Header.module.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <button className={style.button}>Header</button>
+      <div className={style.task}>
+        <input className={style.input} type="text" />
+      </div>
+    </>
+  );
 };
 
 export default Header;
