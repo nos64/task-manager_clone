8740467aadca022d81c7d9d66efbf3c7a8bfa598
Andrew <an.carev4@gmail.com>
2022-11-10 22:03:57 +0300

    feat: implement functions for interacting with columns data

---
+++ src/api/columns.ts
@@ -0,0 +1,29 @@
+import { AxiosError } from 'axios';
+import api from 'servecies';
+import IColumn from 'types/IColumn';
+
+export const getColumnsInBoard = async (boardId: string) => {
+  return await api.get<AxiosError, IColumn[]>(`/boards/${boardId}/columns`);
+};
+
+export const createColumn = async (boardId: string, options: Pick<IColumn, 'title' | 'order'>) => {
+  return await api.post<AxiosError, IColumn>(`/boards/${boardId}/columns`, { ...options });
+};
+
+export const getColumn = async (boardId: string, columnId: string) => {
+  return await api.get<AxiosError, IColumn>(`/boards/${boardId}/columns/${columnId}`);
+};
+
+export const updateColumn = async (
+  boardId: string,
+  columnId: string,
+  options: Pick<IColumn, 'title' | 'order'>
+) => {
+  return await api.put<AxiosError, IColumn>(`/boards/${boardId}/columns/${columnId}`, {
+    ...options,
+  });
+};
+
+export const deleteColumn = async (boardId: string, columnId: string) => {
+  return await api.delete<AxiosError, IColumn>(`/boards/${boardId}/columns/${columnId}`);
+};
