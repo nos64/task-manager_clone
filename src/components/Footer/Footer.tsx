93a3214e13402f6fd0d805a8c3d804d5c82b2f81
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-11-10 23:06:17 +0300

    feat: modify basic styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import styles from './Footer.module.scss';
 import rssImg from './../../assets/images/rs_school_js.svg';
+import Container from 'components/Container';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <div className={`${styles.container} ${styles.footerContainer}`}>
-      <a className="courseLink" href="https://rs.school/react/">
-        <img className={styles.courseImg} src={rssImg} alt="course img" />
-      </a>
-      <ul className={styles.githubList}>
-        <li className={styles.githubItem}>
-          <a className="githubLink" href="https://github.com/andrewkarev">
-            andrewkarev
-          </a>
-        </li>
-        <li className={styles.githubItem}>
-          <a className="githubLink" href="https://github.com/nos64">
-            nos64
-          </a>
-        </li>
-        <li className={styles.githubItem}>
-          <a className="githubLink" href="https://github.com/kritskaya">
-            kritskaya
-          </a>
-        </li>
-      </ul>
-      <div className="copyright">© 2022</div>
-    </div>
+    <Container>
+      <div className={styles.footerContent}>
+        <a className="courseLink" href="https://rs.school/react/">
+          <img className={styles.courseImg} src={rssImg} alt="course img" />
+        </a>
+        <ul className={styles.githubList}>
+          <li className={styles.githubItem}>
+            <a className="githubLink" href="https://github.com/andrewkarev">
+              andrewkarev
+            </a>
+          </li>
+          <li className={styles.githubItem}>
+            <a className="githubLink" href="https://github.com/nos64">
+              nos64
+            </a>
+          </li>
+          <li className={styles.githubItem}>
+            <a className="githubLink" href="https://github.com/kritskaya">
+              kritskaya
+            </a>
+          </li>
+        </ul>
+        <div className="copyright">© 2022</div>
+      </div>
+    </Container>
   </footer>
 );
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { ROUTES } from 'common/routes';
+import Container from 'components/Container';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
@@ -13,24 +14,26 @@ const Header = () => (
       <span className={styles.burgerLine}></span>
       <span className={styles.burgerLine}></span>
     </div>
-    <div className={`${styles.container} ${styles.headerContainer}`}>
-      <div className={styles.logo}>
-        <Link to={ROUTES.WELCOME}>Task Manager</Link>
+    <Container>
+      <div className={`${styles.headerContent}`}>
+        <div className={styles.logo}>
+          <Link to={ROUTES.WELCOME}>Task Manager</Link>
+        </div>
+        <div className={styles.actions}>
+          <button className={styles.createBoardBtn}>
+            <img
+              className={styles.createBoardBtnImg}
+              src="https://cdn.icon-icons.com/icons2/685/PNG/512/plus_icon-icons.com_61187.png"
+              alt="create board img"
+            />
+            Create Board
+          </button>
+          <ThemeToggler />
+          <LangToggler />
+          <Navigation />
+        </div>
       </div>
-      <div className={styles.actions}>
-        <button className={styles.createBoardBtn}>
-          <img
-            className={styles.createBoardBtnImg}
-            src="https://cdn.icon-icons.com/icons2/685/PNG/512/plus_icon-icons.com_61187.png"
-            alt="create board img"
-          />
-          Create Board
-        </button>
-        <ThemeToggler />
-        <LangToggler />
-        <Navigation />
-      </div>
-    </div>
+    </Container>
   </header>
 );
 
