86a5a4e5e2cd274a035bd4901029933963238778
Mikhail Nosov <manosov1984@gmail.com>
2022-11-10 15:18:03 +0400

    feat: add Container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import style from './Header.module.scss';
 
 const Header = () => {
-  return (
-    <>
-      <button className={style.button}>Header</button>
-      <div className={style.task}>
-        <input className={style.input} type="text" />
-      </div>
-    </>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
