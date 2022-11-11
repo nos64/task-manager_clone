126a42a51ed235ad77189b7952d6a9d240139313
Andrew <91889887+andrewkarev@users.noreply.github.com>
2022-11-11 14:56:45 +0300

    feat: implement API logic layer
    * refactor: update `axios` library version
    
    * feat: implement interfaces for api responses
    
    * feat: implement axios instance
    
    * feat: implement functions for authorisation request
    
    * feat: implement functions for interacting with users data
    
    * feat: implement functions for interacting with boards data
    
    * feat: implement functions for interacting with columns data
    
    * feat: implement functions for interacting with tasks data
    
    * refactor: update layout styles file name
    
    * refactor: fix grammatical mistakes

---
+++ src/types/IColumn.ts
@@ -0,0 +1,8 @@
+interface IColumn {
+  _id: string;
+  title: string;
+  order: number;
+  boardId: string;
+}
+
+export default IColumn;
---
+++ src/types/IUser.ts
@@ -0,0 +1,9 @@
+interface IUser {
+  _id: string;
+  name: string;
+  login: string;
+  password: string;
+  token: string;
+}
+
+export default IUser;
