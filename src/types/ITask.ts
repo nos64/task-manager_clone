37eb8f5a5f0467278ac14b1d5820a83d7ffcf15d
Andrew <91889887+andrewkarev@users.noreply.github.com>
2022-11-11 14:56:45 +0300

    feat: implement API logic layer
    * refactor: update `axios` library version
    
    * feat: implement interfaces for api responses
    
    * feat: implement axios instance
    
    * feat: implement functions for authorisation request
    
    * feat: implement functions for interacting with users data
    
    * feat: implement functions for interacting with boards data
    
    * feat: implement functions for interacting with columns data
    
    * feat: implement functions for interacting with tasks data
    
    * refactor: update layout styles file name
    
    * refactor: fix grammatical mistakes

---
+++ src/types/ITask.ts
@@ -0,0 +1,12 @@
+interface ITask {
+  _id: string;
+  title: string;
+  order: number;
+  boardId: string;
+  columnId: string;
+  description: string;
+  userId: string;
+  users: string[];
+}
+
+export default ITask;
