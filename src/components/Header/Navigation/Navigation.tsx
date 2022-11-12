549101072915e50c1ab9691b27d01849d46379c4
Andrew <an.carev4@gmail.com>
2022-11-12 17:21:22 +0300

    feat: apply store values to he `Navigation` component

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -3,10 +3,20 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.scss';
 import userAvatar from './../../../assets/images/user.png';
+import { useAppDispatch, useAppSelector } from 'hooks/redux';
+import { setIsAuthorised } from 'store/reducers/userSlice';
 
 const Navigation = () => {
-  const isAuthorised = true;
   const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const isAuthorised = useAppSelector((state) => state.user.isAuthorised);
+  const userName = useAppSelector((state) => state.user.name);
+  const dispatch = useAppDispatch();
+
+  const handleSignOutClick = () => {
+    dispatch(setIsAuthorised(false));
+    setIsMenuOpened(false);
+  };
 
   return (
     <nav
@@ -16,7 +26,7 @@ const Navigation = () => {
     >
       <div className={styles.user} onClick={() => setIsMenuOpened(!isMenuOpened)}>
         <img className={styles.userAvatar} src={userAvatar} alt="user avatar" />
-        <p className={styles.userName}>Username</p>
+        <p className={styles.userName}>{userName}</p>
       </div>
       <ul className={styles.navList}>
         <li className={styles.navItem}>
@@ -36,7 +46,7 @@ const Navigation = () => {
           </NavLink>
         </li>
         <li className={styles.navItem}>
-          <NavLink className={styles.navLink} to={ROUTES.WELCOME}>
+          <NavLink className={styles.navLink} to={ROUTES.WELCOME} onClick={handleSignOutClick}>
             Sign out
           </NavLink>
         </li>
