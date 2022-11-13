7d7679d762e5f70a250309f92bd6b115179fbb60
Mikhail Nosov <manosov1984@gmail.com>
2022-11-14 00:44:59 +0400

    feat: add base change Avatar

--- src/components/EditProfile/UserPreview/ChangeAvatar/ChangeAvatarContent.tsx
+++ src/components/EditProfile/UserPreview/ChangeAvatar/ChangeAvatarContent.tsx
@@ -1,8 +1,68 @@
-import React from 'react';
-import styles from './ChangeAvatar.module.scss';
+import React, { useState } from 'react';
+import styles from './ChangeAvatarContent.module.scss';
+import userImage0 from '../../../../assets/images/userImage0.png';
+import userImage1 from '../../../../assets/images/userImage1.png';
+import userImage2 from '../../../../assets/images/userImage2.png';
+import userImage3 from '../../../../assets/images/userImage3.png';
+import userImage4 from '../../../../assets/images/userImage4.png';
+import userImage5 from '../../../../assets/images/userImage5.png';
+import userImage6 from '../../../../assets/images/userImage6.png';
+import userImage7 from '../../../../assets/images/userImage7.png';
+import userImage8 from '../../../../assets/images/userImage8.png';
+import userImage9 from '../../../../assets/images/userImage9.png';
 
-const ChangeAvatarContent = () => {
-  return <div></div>;
+interface IChangeAvatarContent {
+  setModalActive: React.Dispatch<React.SetStateAction<boolean>>;
+  setCurrentAvatar: React.Dispatch<React.SetStateAction<string>>;
+  currentAvatar: string;
+}
+const ChangeAvatarContent: React.FC<IChangeAvatarContent> = ({
+  setModalActive,
+  currentAvatar,
+  setCurrentAvatar,
+}) => {
+  const [activeAvatar, setActivAvatar] = useState(currentAvatar);
+  const avatars = [
+    userImage0,
+    userImage1,
+    userImage2,
+    userImage3,
+    userImage4,
+    userImage5,
+    userImage6,
+    userImage7,
+    userImage8,
+    userImage9,
+  ];
+
+  const saveNewAvatar = () => {
+    setCurrentAvatar(activeAvatar);
+    setModalActive(false);
+  };
+  return (
+    <>
+      <div className={styles.imagesWrapper}>
+        {avatars.map((image) => (
+          <div className={styles.imageWrapper} key={image} onClick={() => setActivAvatar(image)}>
+            <img src={image} width={120} alt="User image" />
+          </div>
+        ))}
+      </div>
+      <div className={styles.buttonsWrapper}>
+        <button
+          className={styles.submitBtn}
+          type="button"
+          onClick={saveNewAvatar}
+          disabled={currentAvatar === activeAvatar}
+        >
+          CHANGE
+        </button>
+        <button className={styles.canselBtn} type="button" onClick={() => setModalActive(false)}>
+          CANCEL
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default ChangeAvatarContent;
--- src/components/EditProfile/UserPreview/UserPreview.tsx
+++ src/components/EditProfile/UserPreview/UserPreview.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './UserPreview.module.scss';
-import { FaUserCircle } from 'react-icons/fa';
+import Modal from 'components/Modal';
+import ChangeAvatarContent from './ChangeAvatar';
+import userImage0 from '../../../assets/images/userImage0.png';
 
 const UserPreview = () => {
+  const [modalActive, setModalActive] = useState(false);
+  const [currentAvatar, setCurrentAvatar] = useState(userImage0);
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.avatrWrapper}>
-        <FaUserCircle size={140} color={'#E1E1E1'} />
-        <button className={styles.avatarButon}>Change avatar</button>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.avatrWrapper}>
+          <img src={currentAvatar} width={120} alt="User image" />
+          <button className={styles.avatarButon} type="button" onClick={() => setModalActive(true)}>
+            Change avatar
+          </button>
+        </div>
+        <div className={styles.nameWrapper}>
+          <p className={styles.description}>
+            Name: <span className={styles.value}>RegName</span>
+          </p>
+          <p className={styles.description}>
+            Login: <span className={styles.value}>RegLogin</span>
+          </p>
+        </div>
       </div>
-      <div className={styles.nameWrapper}>
-        <p className={styles.description}>
-          Name: <span className={styles.value}>RegName</span>
-        </p>
-        <p className={styles.description}>
-          Login: <span className={styles.value}>RegLogin</span>
-        </p>
-      </div>
-    </div>
+      <Modal modalActive={modalActive} setModalActive={setModalActive}>
+        <ChangeAvatarContent
+          setModalActive={setModalActive}
+          currentAvatar={currentAvatar}
+          setCurrentAvatar={setCurrentAvatar}
+        />
+      </Modal>
+    </>
   );
 };
 
