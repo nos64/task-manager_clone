d08f1e50c4a568490fe7f42608327318a3cef7ba
Tatyana Kritskaya <37663828+kritskaya@users.noreply.github.com>
2022-11-13 14:16:21 +0300

    feat: implement Main page component
    * feat: implement board component
    
    * feat: implement Main Page component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import { ROUTES } from 'common/routes';
 import Layout from 'components/Layout';
+import BoardsPage from 'pages/BoardsPage';
 import EditProfilePage from 'pages/EditProfilePage';
 import BoardPage from 'pages/BoardPage/BoardPage';
 import React from 'react';
@@ -8,11 +9,11 @@ import { Route, Routes } from 'react-router-dom';
 const App: React.FC = () => (
   <Routes>
     <Route path={ROUTES.WELCOME} element={<Layout />}>
-      {/* <Route index element={<WelcomePage />} />
-      <Route path={ROUTES.SIGN_IN} element={<AuthPage />} />
-      <Route path={ROUTES.SIGN_UP} element={<AuthPage />} /> */}
+      {/* <Route index element={<WelcomePage />} /> */}
+      {/* <Route path={ROUTES.SIGN_IN} element={<AuthPage />} /> */}
+      {/* <Route path={ROUTES.SIGN_UP} element={<AuthPage />} /> */}
       <Route path={ROUTES.PROFILE} element={<EditProfilePage />} />
-      {/* <Route path={ROUTES.BOARDS} element={<BoardsPage />} />*/}
+      <Route path={ROUTES.BOARDS} element={<BoardsPage />} />
       <Route path={ROUTES.BOARD} element={<BoardPage />} />
       {/* <Route path="*" element={<NotFound />} /> */}
     </Route>
---
+++ src/pages/BoardsPage/BoardsPage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Board from './Board';
+import styles from './BoardsPage.module.scss';
+
+const BoardsPage = () => {
+  const boards = [
+    {
+      _id: '1',
+      title: 'Board 111111111111111111111111111111111',
+      description:
+        'board description 11111111111111111111111111111111 1111111111111111111111111111111111111111',
+    },
+    { _id: '2', title: 'Board 2', description: 'board description 2' },
+    { _id: '3', title: 'Board 3', description: 'board description 3' },
+    { _id: '4', title: 'Board 4', description: 'board description 4' },
+    { _id: '5', title: 'Board 5', description: 'board description 5' },
+    { _id: '6', title: 'Board 6', description: 'board description 6' },
+    { _id: '7', title: 'Board 7', description: 'board description 7' },
+  ];
+
+  return (
+    <div className={styles.pageContent}>
+      <h3 className={styles.title}>Boards</h3>
+      <div className={styles.boardsContainer}>
+        {boards.map((board) => {
+          return <Board key={board._id} item={board} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default BoardsPage;
