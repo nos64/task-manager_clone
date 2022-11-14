3921f331daf1538f19ee123a8beeb097799a5c5e
MIKHAIL NOSOV <67101576+nos64@users.noreply.github.com>
2022-11-14 16:53:14 +0400

    Change avatar (#36)
    * feat: add base change Avatar
    
    * feat: add active & current avatar styles
    
    * fix: fix commet on PR
    
    * refactor: change on PR

--- src/components/EditProfile/UserPreview/ChangeAvatar/ChangeAvatarContent.tsx
+++ src/components/EditProfile/UserPreview/ChangeAvatar/ChangeAvatarContent.tsx
@@ -1,8 +1,89 @@
-import React from 'react';
-import styles from './ChangeAvatar.module.scss';
+import React, { useState } from 'react';
+import styles from './ChangeAvatarContent.module.scss';
 
-const ChangeAvatarContent = () => {
-  return <div></div>;
+interface IAvatar {
+  id: number;
+  src: string;
+  isCurrent: boolean;
+  isActive: boolean;
+}
+interface IChangeAvatarContent {
+  setModalActive: React.Dispatch<React.SetStateAction<boolean>>;
+  currentAvatar: IAvatar;
+  setCurrentAvatar: React.Dispatch<React.SetStateAction<IAvatar>>;
+  avatarsArray: IAvatar[];
+  setAvatarArray: React.Dispatch<React.SetStateAction<IAvatar[]>>;
+}
+const ChangeAvatarContent: React.FC<IChangeAvatarContent> = ({
+  setModalActive,
+  currentAvatar,
+  setCurrentAvatar,
+  avatarsArray,
+  setAvatarArray,
+}) => {
+  const [activeAvatar, setActiveAvatar] = useState(currentAvatar);
+
+  const chooseActiveAvatar = (id: number) => {
+    const newAvatarArray = avatarsArray.map((img) =>
+      img.id === id ? { ...img, isActive: true } : { ...img, isActive: false }
+    );
+    setAvatarArray(newAvatarArray);
+    const activeImage = newAvatarArray.find((image) => image.isActive);
+    if (activeImage) setActiveAvatar(activeImage);
+  };
+
+  const saveNewAvatar = () => {
+    const newAvatarArray = avatarsArray.map((img) =>
+      img.isActive === true
+        ? { ...img, isCurrent: true, isActive: false }
+        : { ...img, isCurrent: false, isActive: false }
+    );
+    setAvatarArray(newAvatarArray);
+    const currentImage = newAvatarArray.find((image) => image.isCurrent);
+    if (currentImage) setCurrentAvatar(currentImage);
+    setModalActive(false);
+  };
+  return (
+    <>
+      <div className={styles.imagesWrapper}>
+        {avatarsArray.map((image) => (
+          <span
+            key={image.id.toString()}
+            className={
+              image.isCurrent
+                ? styles.imageWrapper + ' ' + styles.currentAvatar
+                : styles.imageWrapper
+            }
+          >
+            <button
+              className={
+                image.isActive
+                  ? styles.imageWrapper + ' ' + styles.activeAvatar
+                  : styles.imageWrapper
+              }
+              type="button"
+              onClick={() => chooseActiveAvatar(image.id)}
+            >
+              <img src={image.src} width={120} alt={`User image-${image.id}`} />
+            </button>
+          </span>
+        ))}
+      </div>
+      <div className={styles.buttonsWrapper}>
+        <button
+          className={styles.submitBtn}
+          type="button"
+          onClick={saveNewAvatar}
+          disabled={currentAvatar.src === activeAvatar.src}
+        >
+          CHANGE
+        </button>
+        <button className={styles.canselBtn} type="button" onClick={() => setModalActive(false)}>
+          CANCEL
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default ChangeAvatarContent;
--- src/components/EditProfile/UserPreview/UserPreview.tsx
+++ src/components/EditProfile/UserPreview/UserPreview.tsx
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './UserPreview.module.scss';
-import { FaUserCircle } from 'react-icons/fa';
+import Modal from 'components/Modal';
+import ChangeAvatarContent from './ChangeAvatar';
+import { avatars } from 'common/constants';
 import { useAppSelector } from 'hooks/redux';
 
 const UserPreview = () => {
+  const [modalActive, setModalActive] = useState(false);
+  const [avatarsArray, setAvatarArray] = useState(avatars);
+  const [currentAvatar, setCurrentAvatar] = useState(avatars[0]);
   const name = useAppSelector((state) => state.user.name);
   const login = useAppSelector((state) => state.user.login);
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.avatrWrapper}>
-        <FaUserCircle size={140} color={'#E1E1E1'} />
-        <button className={styles.avatarButon}>Change avatar</button>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.avatrWrapper}>
+          <img src={currentAvatar.src} width={120} alt={`User image-${currentAvatar.id}`} />
+          <button className={styles.avatarButon} type="button" onClick={() => setModalActive(true)}>
+            Change avatar
+          </button>
+        </div>
+        <div className={styles.nameWrapper}>
+          <p className={styles.description}>
+            Name: <span className={styles.value}>{name}</span>
+          </p>
+          <p className={styles.description}>
+            Login: <span className={styles.value}>{login}</span>
+          </p>
+        </div>
       </div>
-      <div className={styles.nameWrapper}>
-        <p className={styles.description}>
-          Name: <span className={styles.value}>{name}</span>
-        </p>
-        <p className={styles.description}>
-          Login: <span className={styles.value}>{login}</span>
-        </p>
-      </div>
-    </div>
+      <Modal modalActive={modalActive} setModalActive={setModalActive}>
+        <ChangeAvatarContent
+          setModalActive={setModalActive}
+          currentAvatar={currentAvatar}
+          setCurrentAvatar={setCurrentAvatar}
+          avatarsArray={avatarsArray}
+          setAvatarArray={setAvatarArray}
+        />
+      </Modal>
+    </>
   );
 };
-
 export default UserPreview;
