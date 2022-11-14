31c818b5fea734e5a359bb3896f7b6731c1c50b4
nos64 <manosov1984@gmail.com>
2022-11-14 16:22:49 +0400

    refactor: change on PR

--- src/components/EditProfile/UserPreview/UserPreview.tsx
+++ src/components/EditProfile/UserPreview/UserPreview.tsx
@@ -3,11 +3,15 @@ import styles from './UserPreview.module.scss';
 import Modal from 'components/Modal';
 import ChangeAvatarContent from './ChangeAvatar';
 import { avatars } from 'common/constants';
+import { useAppSelector } from 'hooks/redux';
 
 const UserPreview = () => {
   const [modalActive, setModalActive] = useState(false);
   const [avatarsArray, setAvatarArray] = useState(avatars);
   const [currentAvatar, setCurrentAvatar] = useState(avatars[0]);
+  const name = useAppSelector((state) => state.user.name);
+  const login = useAppSelector((state) => state.user.login);
+
   return (
     <>
       <div className={styles.wrapper}>
@@ -19,10 +23,10 @@ const UserPreview = () => {
         </div>
         <div className={styles.nameWrapper}>
           <p className={styles.description}>
-            Name: <span className={styles.value}>RegName</span>
+            Name: <span className={styles.value}>{name}</span>
           </p>
           <p className={styles.description}>
-            Login: <span className={styles.value}>RegLogin</span>
+            Login: <span className={styles.value}>{login}</span>
           </p>
         </div>
       </div>
@@ -38,5 +42,4 @@ const UserPreview = () => {
     </>
   );
 };
-
 export default UserPreview;
