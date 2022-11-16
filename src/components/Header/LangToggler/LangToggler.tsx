6dedcec5e1d2acc53ca2c7449530c032d8853fbe
Andrew <an.carev4@gmail.com>
2022-11-16 15:31:32 +0300

    feat: stylize `LangToggler`

--- src/components/Header/LangToggler/LangToggler.tsx
+++ src/components/Header/LangToggler/LangToggler.tsx
@@ -1,6 +1,30 @@
+import { useAppDispatch, useAppSelector } from 'hooks/redux';
 import React from 'react';
+import { setLanguage } from 'store/reducers/userSlice';
+import Languages from 'types/Languages';
 import styles from './LangToggler.module.scss';
 
-const LangToggler = () => <div className={styles.langToggler}>RU</div>;
+const LangToggler = () => {
+  const language = useAppSelector((state) => state.user.language);
+  const dispatch = useAppDispatch();
+
+  const handleOptionClick = (language: Languages) => {
+    dispatch(setLanguage(language));
+  };
+
+  return (
+    <div className={styles.langToggler}>
+      <div className={styles.preview}>{language}</div>
+      <ul className={styles.langTogglerContent}>
+        <li className={styles.option} onClick={() => handleOptionClick('EN')}>
+          EN
+        </li>
+        <li className={styles.option} onClick={() => handleOptionClick('RU')}>
+          RU
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default LangToggler;
