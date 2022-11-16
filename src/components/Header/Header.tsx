d022f43ee2f3355af56344cc62495c0e2e110f9f
MIKHAIL NOSOV <67101576+nos64@users.noreply.github.com>
2022-11-16 12:42:48 +0400

    feat: Implement burger menu
    * feat: add animation burger
    
    * feat: add base styles on Burger
    
    * feat: update burger close cby click
    
    * feat: change open burger style
    
    * feat: change open burger style
    
    * feat: update burger styles & transition
    
    * feat: add scrollController
    
    * fix: fix close burger-menu
    
    * fix: fix close burger-menu
    
    * fix: gramatical error
    
    * refactor: resizing icons
    
    * fix: add logo margin-left
    
    * fix: fix logo & langIcon size

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,46 @@
 import { ROUTES } from 'common/routes';
 import Container from 'components/Container';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import LangToggler from './LangToggler';
 import Navigation from './Navigation';
 import ThemeToggler from './ThemeToggler';
 import { GoPlus } from 'react-icons/go';
+import BurgerMenu from './BurgerMenu';
 
-const Header = () => (
-  <header className={styles.header}>
-    <Container>
-      <div className={`${styles.headerContent}`}>
-        <div className={styles.burger}>
-          <span className={styles.burgerLine}></span>
-          <span className={styles.burgerLine}></span>
-          <span className={styles.burgerLine}></span>
-        </div>
-        <div className={styles.logo}>
-          <Link to={ROUTES.WELCOME}>Task Manager</Link>
-        </div>
-        <div className={styles.actions}>
-          <button className={styles.createBoardBtn}>
-            <GoPlus />
-            Create Board
-          </button>
-          <ThemeToggler />
-          <LangToggler />
-          <Navigation />
-        </div>
-      </div>
-    </Container>
-  </header>
-);
+const Header = () => {
+  const [isOpenBurger, setIsOpenBurger] = useState(false);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <Container>
+          <div className={`${styles.headerContent}`}>
+            <div
+              className={isOpenBurger ? styles.burger + ' ' + styles.active : styles.burger}
+              onClick={() => setIsOpenBurger(!isOpenBurger)}
+            >
+              <span className={styles.burgerLine}></span>
+            </div>
+            <div className={isOpenBurger ? styles.logo + ' ' + styles.activeBurger : styles.logo}>
+              <Link to={ROUTES.WELCOME}>Task Manager</Link>
+            </div>
+            <div className={styles.actions}>
+              <button className={styles.createBoardBtn}>
+                <GoPlus />
+                Create Board
+              </button>
+              <ThemeToggler />
+              <LangToggler />
+              <Navigation />
+            </div>
+          </div>
+        </Container>
+      </header>
+      <BurgerMenu isOpenBurger={isOpenBurger} setIsOpenBurger={setIsOpenBurger} />
+    </>
+  );
+};
 
 export default Header;
