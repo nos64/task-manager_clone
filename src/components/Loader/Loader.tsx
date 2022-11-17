52fec255c69403d385db37ec91582753f2bf5d10
Andrew <91889887+andrewkarev@users.noreply.github.com>
2022-11-17 18:58:51 +0300

    feat: implement `Preloader` component
    * refactor: remove loader from the `AuthPage`
    
    * fix: remove unnecessary css rules for `NotFoundPage`
    
    * feat: add items to the scss colors
    
    * feat: implement `Loader` component
    
    * feat: implement logic for displaying loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Loader.module.scss';
+
+const Loader = () => {
+  return (
+    <div className={styles.loaderWrapper}>
+      <div className={styles.loader}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
