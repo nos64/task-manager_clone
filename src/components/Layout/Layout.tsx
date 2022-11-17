7a6f248da2e17bd3cadb7a3d633027ea6825c6be
Andrew <an.carev4@gmail.com>
2022-11-17 17:35:57 +0300

    feat: implement logic for displaying loader

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'components/Container';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer';
 import Header from '../Header';
 import styles from './Layout.module.scss';
+import { useAppSelector } from 'hooks/redux';
+import Loader from 'components/Loader';
+import { scrollController } from 'utils/scrollController';
 
-const Layout: React.FC = () => (
-  <div className={styles.wrapper}>
-    <Header />
-    <main className={styles.main}>
-      <Container>
-        <Outlet />
-      </Container>
-    </main>
-    <Footer />
-  </div>
-);
+const Layout: React.FC = () => {
+  const isUserPending = useAppSelector((state) => state.user.isPending);
+
+  useEffect(() => {
+    isUserPending ? scrollController.disableScroll() : scrollController.enableScroll();
+  }, [isUserPending]);
+
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <main className={styles.main}>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+      {isUserPending && <Loader />}
+    </div>
+  );
+};
 
 export default Layout;
