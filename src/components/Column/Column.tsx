e4ed36a03040643cfe33c91821d0cbe1778edb17
Andrew <an.carev4@gmail.com>
2022-11-20 19:46:07 +0300

    feat: implement `TaskModal` component

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ITask from 'types/ITask';
 import styles from './Column.module.scss';
 import Task from './Task';
@@ -6,6 +6,7 @@ import { RiDeleteBin6Line } from 'react-icons/ri';
 import NewTask from './NewTask/NewTask';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
 import { DndType } from 'common/dnd-types';
+import TaskModal from 'components/TaskModal';
 
 type ColumnProps = {
   item: { _id: string; title: string; tasks: ITask[] };
@@ -13,45 +14,74 @@ type ColumnProps = {
 };
 
 const Column: React.FC<ColumnProps> = ({ item, index }) => {
-  return (
-    <Draggable draggableId={item._id} index={index}>
-      {(dragProvided, snapshot) => (
-        <Droppable droppableId={item._id} type={DndType.TASK}>
-          {(dropProvided, dropSnapshot) => (
-            <div
-              className={`${styles.column} ${snapshot.isDragging ? styles.dragged : ''} ${
-                dropSnapshot.isDraggingOver ? styles.dropped : ''
-              }`}
-              ref={dragProvided.innerRef}
-              {...dragProvided.draggableProps}
-              {...dragProvided.dragHandleProps}
-            >
-              <div className={styles.columnHeader}>
-                <RiDeleteBin6Line className={styles.removeBtn} />
-                <div className={styles.columnColor}></div>
-                <h4 className={styles.title}>{item.title}</h4>
-                <p className={styles.tasksCount}>({item.tasks.length})</p>
-              </div>
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const [modalMode, setModalMode] = useState<'create' | 'edit'>('create');
+  const [selectedTask, setSelectedTask] = useState<ITask | null>(null);
 
-              <div className={styles.columnContent}>
-                <div
-                  className={styles.tasksContainer}
-                  ref={dropProvided.innerRef}
-                  {...dropProvided.droppableProps}
-                >
-                  {item.tasks.map((task, index) => {
-                    return <Task key={task._id} item={task} index={index} />;
-                  })}
-                  {dropProvided.placeholder}
+  const toggleModal = () => {
+    setIsModalOpened((prev) => !prev);
+  };
+
+  return (
+    <>
+      <Draggable draggableId={item._id} index={index}>
+        {(dragProvided, snapshot) => (
+          <Droppable droppableId={item._id} type={DndType.TASK}>
+            {(dropProvided, dropSnapshot) => (
+              <div
+                className={`${styles.column} ${snapshot.isDragging ? styles.dragged : ''} ${
+                  dropSnapshot.isDraggingOver ? styles.dropped : ''
+                }`}
+                ref={dragProvided.innerRef}
+                {...dragProvided.draggableProps}
+                {...dragProvided.dragHandleProps}
+              >
+                <div className={styles.columnHeader}>
+                  <RiDeleteBin6Line className={styles.removeBtn} />
+                  <div className={styles.columnColor}></div>
+                  <h4 className={styles.title}>{item.title}</h4>
+                  <p className={styles.tasksCount}>({item.tasks.length})</p>
                 </div>
 
-                <NewTask />
+                <div className={styles.columnContent}>
+                  <div
+                    className={styles.tasksContainer}
+                    ref={dropProvided.innerRef}
+                    {...dropProvided.droppableProps}
+                  >
+                    {item.tasks.map((task, index) => {
+                      return (
+                        <Task
+                          key={task._id}
+                          item={task}
+                          index={index}
+                          toggleModal={toggleModal}
+                          setModalMode={setModalMode}
+                          setSelectedTask={setSelectedTask}
+                        />
+                      );
+                    })}
+                    {dropProvided.placeholder}
+                  </div>
+
+                  <NewTask
+                    toggleModal={toggleModal}
+                    setModalMode={setModalMode}
+                    setSelectedTask={setSelectedTask}
+                  />
+                </div>
               </div>
-            </div>
-          )}
-        </Droppable>
-      )}
-    </Draggable>
+            )}
+          </Droppable>
+        )}
+      </Draggable>
+      <TaskModal
+        modalActive={isModalOpened}
+        setModalActive={setIsModalOpened}
+        modalMode={modalMode}
+        selectedTask={selectedTask}
+      />
+    </>
   );
 };
 
