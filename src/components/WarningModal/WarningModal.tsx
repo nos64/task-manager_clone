6b18e05d894e7090168f3b54f69419556ef1cc47
Andrew <91889887+andrewkarev@users.noreply.github.com>
2022-11-21 13:00:02 +0300

    refactor: add wrapper to the `WarningModal` component

--- src/components/WarningModal/WarningModal.tsx
+++ src/components/WarningModal/WarningModal.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import styles from './WarningModal.module.scss';
 import { TiWarning } from 'react-icons/ti';
+import Modal from 'components/Modal';
 
 interface WarningModalProps {
   deleteBtnHandler: () => void;
   cancelBtnHandler: () => void;
   message: string;
+  isModalActive: boolean;
 }
 
 const WarningModal: React.FC<WarningModalProps> = ({
   deleteBtnHandler,
   cancelBtnHandler,
   message,
+  isModalActive,
 }) => {
   return (
-    <div className={styles.wrapper}>
-      <TiWarning size={50} color={'#fc4c4c'} />
-      <p className={styles.title}>Do you really want to {message}?</p>
-      <div className={styles.buttonsWrapper}>
-        <button className={styles.deleteBtn} type="button" onClick={deleteBtnHandler}>
-          Delete
-        </button>
-        <button className={styles.submitBtn} type="button" onClick={cancelBtnHandler}>
-          Cancel
-        </button>
+    <Modal modalActive={isModalActive} setModalActive={cancelBtnHandler}>
+      <div className={styles.wrapper}>
+        <TiWarning size={50} color={'#fc4c4c'} />
+        <p className={styles.title}>Do you really want to {message}?</p>
+        <div className={styles.buttonsWrapper}>
+          <button className={styles.deleteBtn} type="button" onClick={deleteBtnHandler}>
+            Delete
+          </button>
+          <button className={styles.submitBtn} type="button" onClick={cancelBtnHandler}>
+            Cancel
+          </button>
+        </div>
       </div>
-    </div>
+    </Modal>
   );
 };
 
