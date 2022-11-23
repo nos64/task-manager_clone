3a09f50c06ceba3746c6c2d787a647da2b755015
Andrew <an.carev4@gmail.com>
2022-11-23 10:51:00 +0300

    feat: apply translation to the `BurgerContentNotauth`

--- src/components/Header/BurgerMenu/BurgerContentNotAuth/BurgerContentNotAuth.tsx
+++ src/components/Header/BurgerMenu/BurgerContentNotAuth/BurgerContentNotAuth.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './BurgerContentNotAuth.module.scss';
 import { ROUTES } from 'common/routes';
+import { useTranslation } from 'react-i18next';
 
 interface IBurgerContentNotAuthProps {
   setIsOpenBurger: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const BurgerContentNotAuth: React.FC<IBurgerContentNotAuthProps> = ({ setIsOpenBurger }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.menuContent}>
       <ul className={styles.navList}>
         <li className={styles.navItem} onClick={() => setIsOpenBurger(false)}>
-          <NavLink to={ROUTES.SIGN_IN}>Sign in</NavLink>
+          <NavLink to={ROUTES.SIGN_IN}>{t('signIn')}</NavLink>
         </li>
         <li className={styles.navSeparator}></li>
         <li className={styles.navItem}>
           <NavLink to={ROUTES.SIGN_UP} onClick={() => setIsOpenBurger(false)}>
-            Sign up
+            {t('signUp')}
           </NavLink>
         </li>
       </ul>
