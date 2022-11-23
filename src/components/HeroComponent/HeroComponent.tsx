8969e5a4cb2f0b69a222b77c89edcaf914641528
nos64 <manosov1984@gmail.com>
2022-11-23 10:32:35 +0400

    feat: start hero component task

---
+++ src/components/HeroComponent/HeroComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from 'HeroComponent.module.scss';
+
+const HeroComponent = () => {
+  return (
+    <div>
+      <h2 className={styles.title}>Project managment app</h2>
+    </div>
+  );
+};
+
+export default HeroComponent;
---
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -0,0 +1,13 @@
+import HeroComponent from 'components/HeroComponent';
+import React from 'react';
+import styles from './WelcomePage.module.scss';
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <HeroComponent />
+    </div>
+  );
+};
+
+export default WelcomePage;
