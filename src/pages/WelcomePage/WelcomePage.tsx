ffbdb7353d5490cffdb4b7e5e4d6039f8763bf13
MIKHAIL NOSOV <67101576+nos64@users.noreply.github.com>
2022-11-23 15:53:23 +0400

    feat: implement Hero component
    * feat: start hero component task
    
    * feat: add base heroPage
    
    * fix: del image link
    
    * fix: comment unused variable
    
    * refactor: delete unused styles

---
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -0,0 +1,12 @@
+import HeroComponent from 'components/HeroComponent';
+import React from 'react';
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <HeroComponent />
+    </div>
+  );
+};
+
+export default WelcomePage;
