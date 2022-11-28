5bed73927687e3ace0b88d6ab361b3a2b41ce329
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-11-28 13:59:22 +0300

    feat: modify tasks dnd behavior

--- src/store/reducers/columnSlice.ts
+++ src/store/reducers/columnSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { updateBoard } from 'api/boards';
 import { createTask, deleteTask, getTasksInColumn, updateTask, updateTasksSet } from 'api/tasks';
 import { AxiosError } from 'axios';
@@ -227,14 +227,17 @@ export const updateTasksOrder = createAsyncThunk(
   'column/updateTasksOrder',
   async (
     data: { tasks: ITask[]; oldColumnId: string; newColumnId: string },
-    { rejectWithValue }
+    { rejectWithValue, dispatch }
   ) => {
-    const tasksData = data.tasks.map((item) => ({
-      _id: item._id,
-      order: item.order,
-      columnId: item.columnId,
-    }));
     try {
+      dispatch(setTasks({ columnId: data.newColumnId, tasks: data.tasks }));
+
+      const tasksData = data.tasks.map((item) => ({
+        _id: item._id,
+        order: item.order,
+        columnId: item.columnId,
+      }));
+
       const newTasks = await updateTasksSet(tasksData);
 
       return { tasks: newTasks, oldColumnId: data.oldColumnId, newColumnId: data.newColumnId };
@@ -254,6 +257,10 @@ export const columnSlice = createSlice({
   reducers: {
     resetColumnTokenExpiration(state) {
       state.isTokenExpired = false;
+    },
+    setTasks(state, action: PayloadAction<{ columnId: string; tasks: ITask[] }>) {
+      const { columnId, tasks } = action.payload;
+      state.tasks[columnId] = tasks;
     },
   },
   extraReducers: (builder) => {
@@ -373,4 +380,4 @@ export const columnSlice = createSlice({
 });
 
 export default columnSlice.reducer;
-export const { resetColumnTokenExpiration } = columnSlice.actions;
+export const { resetColumnTokenExpiration, setTasks } = columnSlice.actions;
