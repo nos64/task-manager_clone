b6f3798dc173c7c7497aa7a3b386eb8a58d33801
Mikhail Nosov <manosov1984@gmail.com>
2022-12-01 21:51:02 +0400

    fix: change border board column task

---
+++ src/index.d.ts
@@ -0,0 +1,22 @@
+// declare module '*.module.scss';
+
+// declare module '*.svg';
+
+// declare module '*.jpeg';
+
+// declare module '*.jpg';
+
+// declare module '*.png';
+
+// declare module '*.webp';
+
+// declare module '*.gif';
+
+// declare module '*.mp3';
+
+// declare module '*.mp4';
+
+declare module '*.mp4' {
+  const src: string;
+  export default src;
+}
