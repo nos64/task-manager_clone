9fe9f7043afc636ae697e73ea740391ab3024519
Andrew <91889887+andrewkarev@users.noreply.github.com>
2022-12-09 19:50:31 +0300

    fix: fix error occur on column deletion

--- src/store/reducers/boardsSlice.ts
+++ src/store/reducers/boardsSlice.ts
@@ -1,7 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { createBoard, deleteBoard, findBoard, getUserRelatedBoards, updateBoard } from 'api/boards';
-import { deleteColumn, getColumnsInBoard } from 'api/columns';
-import { deleteTask, getTasksInColumn } from 'api/tasks';
 import { AxiosError } from 'axios';
 import StatusCodes from 'common/statusCodes';
 import { BoardPick } from 'types/APIModel';
@@ -46,16 +44,8 @@ export const deleteBoardById = createAsyncThunk(
     try {
       await checkBoardExistence(boardId);
 
-      const boardColumns = await getColumnsInBoard(boardId);
-      for (const column of boardColumns) {
-        const columnTasks = await getTasksInColumn(boardId, column._id);
-        columnTasks.forEach((item) => {
-          deleteTask(item.boardId, item.columnId, item._id);
-        });
-        await deleteColumn(boardId, column._id);
-      }
-
       const deletedBoard = await deleteBoard(boardId);
+
       return deletedBoard;
     } catch (error) {
       if (error instanceof AxiosError) {
